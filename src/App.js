74f0f30f28a253046127647a09e94d6fb0938a01
Pamela Badermann <badermann.pamela@gmail.com>
2021-09-20 20:56:51 -0300

    aula 16, elevação de state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React,{ useState } from 'react';
+import Nota from './components/Nota'
+import Resultado from './components/Resultado'
 
 export default function App() {
 
-  const [form, setForm] = useState({"nome":"","curso":"","ano":""})
-
-  const handleFormChange=(e)=>{
-    if (e.target.getAttribute('name')==='fnome') {
-      setForm({"nome":e.target.value,"curso":form.curso,"ano":form.ano})
-    } else if (e.target.getAttribute('name')==='fcurso') {
-      setForm({"nome":form.nome,"curso":e.target.value,"ano":form.ano})
-    } else if (e.target.getAttribute('name')==='fano') {
-      setForm({"nome":form.nome,"curso":form.curso,"ano":e.target.value})
+  const [notas,setNotas] = useState({"nota1":"0","nota2":"0","nota3":"0","nota4":"0"})
+  const handleSetNotas=(e)=>{
+    if(e.target.getAttribute('name')==='nota1') {
+      setNotas({"nota1":e.target.value,"nota2":notas.nota2,"nota3":notas.nota3,"nota4":notas.nota4})
+    } else if(e.target.getAttribute('name')==='nota2') {
+      setNotas({"nota1":notas.nota1,"nota2":e.target.value,"nota3":notas.nota3,"nota4":notas.nota4})
+    } else if(e.target.getAttribute('name')==='nota3') {
+      setNotas({"nota1":notas.nota1,"nota2":notas.nota2,"nota3":e.target.value,"nota4":notas.nota4})
+    } else if(e.target.getAttribute('name')==='nota4') {
+      setNotas({"nota1":notas.nota1,"nota2":notas.nota2,"nota3":notas.nota3,"nota4":e.target.value})
     }
   }
 
   return (
     <>
-      <label>Nome</label>
-      <input type='text' name='fnome' value={form.nome} onChange={(e)=>handleFormChange()}/>
-      <label>Curso</label>
-      <input type='text' name='fcurso' value={form.curso} onChange={(e)=>handleFormChange()}/>
-      <label>Ano</label>
-      <input type='text' name='fano' value={form.ano} onChange={(e)=>handleFormChange()}/>
-      
-      <p>Nome digitado: {form.nome}</p>
-      <p>Curso digitado: {form.curso}</p>
-      <p>Ano digitado: {form.ano}</p>
+      <Nota num={1} nome={'nota1'} nota={notas.nota1} setNota={handleSetNotas}/>
+      <Nota num={2} nome={'nota2'} nota={notas.nota2} setNota={handleSetNotas}/>
+      <Nota num={3} nome={'nota3'} nota={notas.nota3} setNota={handleSetNotas}/>
+      <Nota num={4} nome={'nota4'} nota={notas.nota4} setNota={handleSetNotas}/>
+      <Resultado somaNotas={parseFloat(notas.nota1)+parseFloat(notas.nota2)+parseFloat(notas.nota3)+parseFloat(notas.nota4)}/>
     </>
   );
 }
