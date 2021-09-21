319937e4d274db3c77fb9cd7b134dcf627664c10
Pamela Badermann <badermann.pamela@gmail.com>
2021-09-21 12:44:39 -0300

    aula 25, ciclo de vida de componentes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Carro from './components/Carro'
 
 export default function App() {
 
+  const [carro, setCarro] = useState(true)
+  const mostrarOcultarCarro=()=>{
+    setCarro(!carro)
+  }
+
   return (
     <>
       <h1>Componentes de Classe</h1>
-      <Carro fator={10}/>
+      {carro ? <Carro fator={10}/> : ''}
+      <button onClick={()=>mostrarOcultarCarro()}>
+        Ocultar
+      </button>
     </>
   );
 }
-
--- src/components/Carro.js
+++ src/components/Carro.js
@@ -8,19 +8,18 @@ export default class Carro extends React.Component{
       ligado: false,
       velAtual: 0,
     }
+    this.ld=this.ligarDesligar.bind(this)
   }
 
   ligarDesligar() {
-    // this.state.ligado = true - não renderiza a tela
-    // this.setState({ligado:!this.state.ligado})
-
-    this.setState(
-      (state)=>(
-        {ligado: !state.ligado}
-      )
-    )
+    this.setState({ligado:!this.state.ligado})
+    // this.setState(
+    //   (state)=>(
+    //     {ligado: !state.ligado}
+    //   )
+    // )
   }
-
+  /*
   acelerar(){
     this.setState(
       (state,props)=>(
@@ -28,17 +27,18 @@ export default class Carro extends React.Component{
       )
     )
   }
-
-  /*
- // método tradicional:
-  acelerar(){
-    this.setState(
-      (state,props)=>(
-        {velAtual:this.state.velAtual + this.props.fator}
-      )
-    )
-  }
   */
+  componentDidMount () {
+    console.log('O carro foi criado')
+  }
+
+  componentDidUpdate () {
+    console.log('O carro foi atualizado')
+  }
+
+  componentWillUnmount () {
+    console.log('O carro foi removido')
+  }
 
   render(){
     return(
@@ -47,7 +47,7 @@ export default class Carro extends React.Component{
         <p>Modelo: {this.modelo}</p>
         <p>Ligado: {this.state.ligado ? 'Sim' : 'Não'}</p>
         <p>Vel. Atual: {this.state.velAtual}</p>
-        <button onClick={()=>this.ligarDesligar()}>
+        <button onClick={this.ld}>
           {this.state.ligado ? 'Desligar Carro' : 'Ligar Carro'}
         </button>
         <button onClick={()=>this.acelerar()}>
