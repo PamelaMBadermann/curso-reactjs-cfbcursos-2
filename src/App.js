6931bfdfefee256e3845d89506f6535f2aaf1f6e
Pamela Badermann <badermann.pamela@gmail.com>
2021-09-20 23:06:50 -0300

    aula 21, componentes de classe

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, {useState} from 'react';
+import Classe from './components/Classe'
 
 export default function App() {
 
-  const [nome,setNome] = useState()
-
-  const armazenar=(chave,valor)=> {
-    localStorage.setItem(chave,valor)
-  }
-
-  const consultar=(chave)=> {
-    alert(localStorage.getItem(chave))
-  }
-
-  const apagar=(chave)=> {
-    localStorage.removeItem(chave)
-  }
-
   return (
     <>
-      <label>Digite um nome</label>
-      <input type='text' value={nome} onChange={(e)=>setNome(e.target.value)}/>
-      <button onClick={()=>armazenar('ls_nome', nome)}>Gravar Nome</button>
-      <button onClick={()=>consultar('ls_nome', nome)}>Ver Nome</button>
-      <button onClick={()=>apagar('ls_nome', nome)}>Remover Nome</button>
+      <h1>Componentes de Classe</h1>
+      <Classe canal="CFB Cursos"/>
     </>
   );
 }
---
+++ src/components/Classe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Classe extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Primeiro Componente de Classe</h1>
+        <p>Canal: {this.props.canal}</p>
+      </div>
+    )
+  }
+}
